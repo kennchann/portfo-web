6c8dbdd083322e66cdd5bc3f402662aade7b5357
kennchann <chankamho814@gmail.com>
2023-11-10 23:48:37 +0800

    link fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from './components/hero'
 import Excerpt from './components/excerpt'
 import Work from './components/work'
+import Nav from './components/nav.tsx'
 
 function App() {
   return (
     <>
+    <Nav />
     <Hero />
     <Excerpt />
     <Work />
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Nav from './components/nav.tsx'
 import Footer from './components/footer.tsx'
 import "./_assets/css/main.scss";
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Nav />
+  <BrowserRouter>
     <App />
     <Footer />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
