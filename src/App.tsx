14e58d902f88d0faf6243ff99ace44e57c5dcc77
kennchann <chankamho814@gmail.com>
2023-11-17 12:58:08 +0800

    fix link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Nav from './components/nav.tsx'
+import Footer from './components/footer.tsx'
 import Home from './pages/home';
 import Work from './pages/work';
 import Nuon from './pages/nuon';
@@ -11,16 +13,18 @@ import Dogily from './pages/dogily';
 function App() {
   return (
     <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/nuon" element={<Nuon />} />
-        <Route path="/culture-homes" element={<CultureHomes />} />
-        <Route path="/trusted-node" element={<TNode />} />
-        <Route path="/skyteam" element={<Skyteam />} />
-        <Route path="/cfss-gradbook" element={<GradBook />} />
-        <Route path="/dogily" element={<Dogily />} />
-      </Routes>
+      <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/nuon" element={<Nuon />} />
+          <Route path="/culture-homes" element={<CultureHomes />} />
+          <Route path="/trusted-node" element={<TNode />} />
+          <Route path="/skyteam" element={<Skyteam />} />
+          <Route path="/cfss-gradbook" element={<GradBook />} />
+          <Route path="/dogily" element={<Dogily />} />
+        </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Nav from './components/nav.tsx'
 import App from './App.tsx'
-import Footer from './components/footer.tsx'
 import "./assets/css/main.scss";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Nav />
     <App />
-    <Footer />
   </React.StrictMode>,
 )
