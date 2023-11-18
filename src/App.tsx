31e31ad0421f9727dcd0ee7ba1bbc7f6f6696cad
kennchann <chankamho814@gmail.com>
2023-11-19 02:33:10 +0800

    fix link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-import Nav from './components/nav.tsx'
-import Footer from './components/footer.tsx'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import Nav from './components/nav.tsx';
+import Footer from './components/footer.tsx';
+
 import Home from './pages/home';
 import Work from './pages/work';
 import Nuon from './pages/nuon';
@@ -10,21 +12,32 @@ import Skyteam from './pages/skyteam';
 import GradBook from './pages/cfss-gradbook';
 import Dogily from './pages/dogily';
 
+function ScrollToTopOnRouteChange() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
+      <ScrollToTopOnRouteChange />
       <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/nuon" element={<Nuon />} />
-          <Route path="/culture-homes" element={<CultureHomes />} />
-          <Route path="/trusted-node" element={<TNode />} />
-          <Route path="/skyteam" element={<Skyteam />} />
-          <Route path="/cfss-gradbook" element={<GradBook />} />
-          <Route path="/dogily" element={<Dogily />} />
-          <Route path="*" element={<Work />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/nuon" element={<Nuon />} />
+        <Route path="/culture-homes" element={<CultureHomes />} />
+        <Route path="/trusted-node" element={<TNode />} />
+        <Route path="/skyteam" element={<Skyteam />} />
+        <Route path="/cfss-gradbook" element={<GradBook />} />
+        <Route path="/dogily" element={<Dogily />} />
+        <Route path="*" element={<Work />} />
+      </Routes>
       <Footer />
     </>
   );
